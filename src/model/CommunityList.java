934ad68ffd28a1d59bf9f8678ebef6e54a30993f
Harshit Prajapati <parajapati.ha@northeastern.edu>
2022-10-29 00:11:45 -0400

    commit model changes

---
+++ src/model/CommunityList.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author dell
+ */
+public class CommunityList {
+    
+    private ArrayList<Community> communityList;
+
+    public ArrayList<Community> getCommunityList() {
+        return communityList;
+    }
+
+    public void setCommunityList(ArrayList<Community> communityList) {
+        this.communityList = communityList;
+    }
+    
+    
+    
+}
